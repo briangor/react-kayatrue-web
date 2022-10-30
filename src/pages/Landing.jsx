089cd64c3da266d8c8b9657c98a05552a312e819
briangor <briangor13@gmail.com>
2022-10-30 12:36:48 +0300

    add Landing page

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,20 @@
+import bg from '../images/a.jpeg'
+
+let Landing = () => {
+    return (
+        <>
+        <div className="landing mt-5">
+            {/* <img src={bg} alt="etienne beauregard riverin unsplash" className='bg-img'/> */}
+
+            <div className="row">
+                <p className='hero-title'>Search for your next home with ease</p>
+            </div>
+        </div>
+        <div className="landing-body">
+            How it works
+        </div>
+        </>
+    )
+}
+
+export default Landing;
