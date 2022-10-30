b8f2aca4ff66872bca013cb8fa9878b911499249
briangor <briangor13@gmail.com>
2022-10-30 12:37:33 +0300

    Add Footer section

---
+++ src/components/Footer.jsx
@@ -0,0 +1,10 @@
+let Footer = () => {
+    return (
+        <div className="footer">
+            <p>@2022 Kayatrue</p>
+            <p>Legal Stuff | Privacy Policy</p>
+        </div>
+    )
+}
+
+export default Footer;
