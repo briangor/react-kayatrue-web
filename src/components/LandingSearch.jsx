6a85d1e45956e8911625d94c21e526ecab86d25d
briangor <briangor13@gmail.com>
2022-12-01 20:46:18 +0300

    create search component

---
+++ src/components/LandingSearch.jsx
@@ -0,0 +1,10 @@
+const LandingSearch = () => {
+    return ( 
+        <div className="search">
+            <input type="text" placeholder="Search" className='search-input'/>
+            <button className='btn btn-primary'>Search</button>
+        </div>
+     );
+}
+ 
+export default LandingSearch;
